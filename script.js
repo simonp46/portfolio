76bd8657d7d4596ed1f6031094fecacf9b1ae086
Saimon <theputasboy@gmail.com>
2026-04-09 23:57:15 -0500

    Add Clinical Profile Agent demo card

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function hamburg(){
     const navbar = document.querySelector('.dropdown');
-    navbar.style.transform = 'translateY(0px)';
+    if (navbar) {
+        navbar.style.transform = 'translateY(0px)';
+    }
 }
 
 function cancel(){
-    const navbar = document.querySelector('.cancel');
-    navbar.style.transform = 'translateY(-500px)';
+    const navbar = document.querySelector('.dropdown');
+    if (navbar) {
+        navbar.style.transform = 'translateY(-500px)';
+    }
 }
 
 const texts = [
@@ -15,49 +19,66 @@ const texts = [
     "DATABASE DEVELOPER"
 ]
 
-let speed =100;
+let speed = 100;
 const textElement = document.querySelector('.typewriter-text');
 
 let textIndex = 0;
 let charcterIndex = 0;
 
 function typeWriter(){
-    if(charcterIndex < texts[textIndex].length){
+    if (!textElement) {
+        return;
+    }
+
+    if (charcterIndex < texts[textIndex].length) {
         textElement.innerHTML += texts[textIndex].charAt(charcterIndex);
         charcterIndex++;
         setTimeout(typeWriter, speed);
     }
-    else{
+    else {
         setTimeout(eraseText, 1000);
     }
 }
 
 function eraseText(){
-    if(textElement.innerHTML.length > 0){
-        textElement.innerHTML = textElement.innerHTML.slice(0,- 1);
+    if (!textElement) {
+        return;
+    }
+
+    if (textElement.innerHTML.length > 0) {
+        textElement.innerHTML = textElement.innerHTML.slice(0, -1);
         setTimeout(eraseText, 50);
     }
-    else{
+    else {
         textIndex = (textIndex + 1) % texts.length;
         charcterIndex = 0;
         setTimeout(typeWriter, 500);
     }
-    }
-window.onload = typeWriter;
+}
 
-document.getElementById('hireButton').addEventListener('click', function() {
-    const phoneNumber = '573045374560';
-    const message = "Hello Simon. I have seen your portfolio and would like to talk to you about a job offer.";
-    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    
-    window.open(url, '_blank');
-});
+window.onload = function() {
+    if (textElement) {
+        typeWriter();
+    }
+};
+
+const hireButton = document.getElementById('hireButton');
+
+if (hireButton) {
+    hireButton.addEventListener('click', function() {
+        const phoneNumber = '573045374560';
+        const message = "Hello Simon. I have seen your portfolio and would like to talk to you about a job offer.";
+        const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+        window.open(url, '_blank');
+    });
+}
 
 function sendEmail() {
     const email = "padrongavi@hotmail.com";
     const subject = "job vacancy";
     const body = "Hello Simon. I would like to make you a job offer";
     const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-    
+
     window.open(mailtoLink, '_blank');
 }
